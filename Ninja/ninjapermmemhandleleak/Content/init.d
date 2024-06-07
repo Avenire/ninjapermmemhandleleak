fc6d6183a9591cabe0efa0b17d3ae0f1fba80024
Avenire <kamilnowodworski@gmail.com>
2024-06-07 16:20:32 +0200

    Basic repro: smash V button 15+ times and reload the game - some prints stay on screen permanently :(

--- Ninja/ninjapermmemhandleleak/Content/init.d
+++ Ninja/ninjapermmemhandleleak/Content/init.d
@@ -1,9 +1,18 @@
 /*
  * Initialization function called by Ninja after "Init_Global" (G2) / "Init_<Levelname>" (G1)
  */
+
+var int Ninja_ninjapermmemhandleleak_toggleCnt;
+
+func void Ninja_ninjapermmemhandleleak_RunLoop() {
+    if MEM_KeyState(KEY_V) == KEY_PRESSED {
+        var string msg; msg = ConcatStrings("Hello world ", IntToString(Ninja_ninjapermmemhandleleak_toggleCnt));
+        PrintS_Ext(msg, RGBA(0, 255, 0, 255));
+        Ninja_ninjapermmemhandleleak_toggleCnt += 1;
+
+    };
+ };
 func void Ninja_ninjapermmemhandleleak_Init() {
-    LeGo_MergeFlags( /* DESIRED LEGO PACKAGES */ );
-
-    // WRITE YOUR INITIALIZATIONS HERE
-
+    LeGo_MergeFlags(LeGo_FrameFunctions | LeGo_Interface | LeGo_PrintS | LeGo_Timer);  
+    FF_ApplyOnceGT(Ninja_ninjapermmemhandleleak_RunLoop);
 };
